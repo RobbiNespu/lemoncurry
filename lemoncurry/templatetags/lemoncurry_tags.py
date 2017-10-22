afcf101d095a5550bc46f92bf491acfa0f84ce0e
Danielle McLean <dani@00dani.me>
2017-10-23 10:35:29 +1100

    Add footer with handy info

--- lemoncurry/templatetags/lemoncurry_tags.py
+++ lemoncurry/templatetags/lemoncurry_tags.py
@@ -1,7 +1,18 @@
+import json
+from os.path import join
+
 from django import template
 from django.conf import settings
 
 register = template.Library()
+
+
+@register.simple_tag
+def get_package_json():
+    with open(join(settings.BASE_DIR, 'package.json')) as f:
+        package = json.load(f)
+    return package
+
 
 @register.simple_tag
 def site_name():
