fe75a5755094e1fce9e4ca5dd12f1cb4213d70c1
Danielle McLean <dani@00dani.me>
2017-10-23 10:29:12 +1100

    Add a Stylus stylesheet for the layout, which is automatically compiled by Django Compressor

--- lemoncurry/settings/base.py
+++ lemoncurry/settings/base.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'compressor',
+
     'lemoncurry',
     'home',
     'users',
@@ -124,6 +126,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
+
+COMPRESS_PRECOMPILERS = (
+    ('text/stylus', os.path.join(BASE_DIR, 'node_modules', '.bin', 'stylus') + ' {infile} -o {outfile}'),
+)
 
 
 # Settings specific to lemoncurry
