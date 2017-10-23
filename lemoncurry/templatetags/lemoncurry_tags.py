f0846d8c733855e422a7b67b30e8f1eaa2811ab8
Danielle McLean <dani@00dani.me>
2017-10-23 13:08:11 +1100

    Cache the contents of package.json once loaded, instead of reading the file every time it's required

--- lemoncurry/templatetags/lemoncurry_tags.py
+++ lemoncurry/templatetags/lemoncurry_tags.py
@@ -1,17 +1,21 @@
 import json
 from os.path import join
+from types import SimpleNamespace
 
 from django import template
 from django.conf import settings
 
 register = template.Library()
+cache = SimpleNamespace(package_json=None)
 
 
 @register.simple_tag
 def get_package_json():
+    if cache.package_json:
+        return cache.package_json
     with open(join(settings.BASE_DIR, 'package.json')) as f:
-        package = json.load(f)
-    return package
+        cache.package_json = json.load(f)
+    return cache.package_json
 
 
 @register.simple_tag
