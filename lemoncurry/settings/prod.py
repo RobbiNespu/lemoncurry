2c42a69f1925744400528c13ef22d87ea1e7030a
Danielle McLean <dani@00dani.me>
2017-10-23 17:45:32 +1100

    Set the production SECRET_KEY from the environment

--- lemoncurry/settings/prod.py
+++ lemoncurry/settings/prod.py
@@ -1,12 +1,18 @@
+from os import environ
+
 from .base import *
 from .base import DATABASES
 
 ALLOWED_HOSTS = ['00dani.me']
+DEBUG = False
+SECRET_KEY = environ['DJANGO_SECRET_KEY']
+
+# Use Postgres instead of SQLite in production.
 DATABASES['default'] = {
     'ENGINE': 'django.db.backends.postgresql',
     'NAME': 'lemoncurry',
     'USER': 'lemoncurry',
 }
-DEBUG = False
+
 STATIC_URL = 'https://cdn.00dani.me/'
 MEDIA_URL = STATIC_URL + 'media/'
