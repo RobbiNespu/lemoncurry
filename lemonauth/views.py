492ba744fc938c6083ef66e8326d9cbc08ccdc9a
Danielle McLean <dani@00dani.me>
2017-10-25 00:55:38 +1100

    Adjust breadcrumb label for lemonauth:login to match other labels

--- lemonauth/views.py
+++ lemonauth/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import views as auth_views
 from lemoncurry import breadcrumbs
 
-breadcrumbs.add(route='lemonauth:login', label='login', parent='home:index')
+breadcrumbs.add(route='lemonauth:login', label='log in', parent='home:index')
 
 login = auth_views.LoginView.as_view(
     template_name='lemonauth/login.html',
