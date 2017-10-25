60cd6c911b45173ff627e4d08e952b6ee54e596f
Danielle McLean <dani@00dani.me>
2017-10-25 12:55:24 +1100

    Add entries to the homepage, and make style tweaks so everywhere looks right

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from users.models import User
+from entries.models import Entry
 from lemoncurry import breadcrumbs
 
 breadcrumbs.add('home:index', 'home')
@@ -7,7 +8,9 @@ breadcrumbs.add('home:index', 'home')
 
 def index(request):
     user = get_object_or_404(User, pk=1)
+    entries = Entry.objects.filter(author=user)
     return render(request, 'home/index.html', {
         'user': user,
+        'entries': entries,
         'meta': user.as_meta(request),
     })
