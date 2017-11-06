5c643299c77275aa331befa723aa63a8e2313f50
Danielle McLean <dani@00dani.me>
2017-11-06 16:06:25 +1100

    Permit span[class] in bleached content

--- lemoncurry/templatetags/bleach.py
+++ lemoncurry/templatetags/bleach.py
@@ -5,11 +5,12 @@ from django.utils.safestring import mark_safe
 from bleach.sanitizer import Cleaner, ALLOWED_TAGS
 from bleach.linkifier import LinkifyFilter
 
-tags = ['code', 'p', 'img']
+tags = ['code', 'p', 'img', 'span']
 tags.extend(ALLOWED_TAGS)
 attributes = {
     'a': ('href', 'title', 'class'),
     'img': ('alt', 'src', 'title'),
+    'span': ('class',),
 }
 
 register = template.Library()
