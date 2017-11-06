475302eb079a030bf3754bab11564f2d4c55c0b2
Danielle McLean <dani@00dani.me>
2017-11-06 11:49:43 +1100

    Forgot to urlencode the list of URLs posted up to Superfeedr

--- entries/jobs.py
+++ entries/jobs.py
@@ -1,11 +1,12 @@
 import requests
 from django.conf import settings
 from django_rq import job
+from urllib.parse import urlencode
 
 
 @job
 def ping_hub(*urls):
     requests.post(settings.PUSH_HUB, data={
         'hub.mode': 'publish',
-        'hub.url': ','.join(urls),
+        'hub.url': ','.join(map(urlencode, urls)),
     })
