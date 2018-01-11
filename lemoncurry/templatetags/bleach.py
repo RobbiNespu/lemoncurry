3e4f55fa9cad2f7b9e7b803e2f545f4dd446ad00
Danielle McLean <dani@00dani.me>
2018-01-11 11:41:36 +1100

    Add <cite> to ALLOWED_TAGS, because honestly

--- lemoncurry/templatetags/bleach.py
+++ lemoncurry/templatetags/bleach.py
@@ -5,7 +5,7 @@ from django.utils.safestring import mark_safe
 from bleach.sanitizer import Cleaner, ALLOWED_TAGS
 from bleach.linkifier import LinkifyFilter
 
-tags = ['code', 'p', 'pre', 'img', 'span']
+tags = ['cite', 'code', 'p', 'pre', 'img', 'span']
 tags.extend(ALLOWED_TAGS)
 attributes = {
     'a': ('href', 'title', 'class'),
