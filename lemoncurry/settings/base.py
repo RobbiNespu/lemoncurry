45daf529f87e6d2441607d03659d96d291ed8b66
Danielle McLean <dani@00dani.me>
2018-02-06 16:18:15 +1100

    Switch the preferred password hash from PBKDF2 to the newer and more secure Argon2

--- lemoncurry/settings/base.py
+++ lemoncurry/settings/base.py
@@ -157,6 +157,16 @@ DATABASES = {
 
 AUTH_USER_MODEL = 'users.User'
 
+# Password hashers
+# https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+]
+
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
