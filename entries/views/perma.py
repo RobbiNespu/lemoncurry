039b6a19145af78c44907f7e07a727f341d5a387
Danielle McLean <dani@00dani.me>
2018-03-08 14:13:45 +1100

    Make the permalink views 404 if you try to load a non-existent entry

--- entries/views/perma.py
+++ entries/views/perma.py
@@ -1,11 +1,11 @@
 from annoying.decorators import render_to
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404
 from ..models import Entry
 
 
 @render_to('entries/entry.html')
 def entry(request, kind, id, slug=None):
-    entry = Entry.objects.get(pk=id)
+    entry = get_object_or_404(Entry, pk=id)
     if request.path != entry.url:
         return redirect(entry.url, permanent=True)
     return {
@@ -17,7 +17,7 @@ def entry(request, kind, id, slug=None):
 
 @render_to('entries/entry_amp.html')
 def entry_amp(request, kind, id, slug=None):
-    entry = Entry.objects.get(pk=id)
+    entry = get_object_or_404(Entry, pk=id)
     if request.path != entry.amp_url:
         return redirect(entry.amp_url, permanent=True)
     return {'entry': entry}
