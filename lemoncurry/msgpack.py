142e3eff2bf75c60d8a26714ba238ae165e57d62
Danielle McLean <dani@00dani.me>
2018-05-28 21:33:20 +1000

    Ugh. Fix the new MessagePack serialiser to interface reliably with the msgpack library (gotta pass some flags and stuff) - also, actually depend on msgpack to make sure we have it

--- lemoncurry/msgpack.py
+++ lemoncurry/msgpack.py
@@ -5,7 +5,7 @@ from django_redis.serializers.base import BaseSerializer
 
 class MSGPackModernSerializer(BaseSerializer):
     def dumps(self, value):
-        return msgpack.dumps(value)
+        return msgpack.packb(value, use_bin_type=True)
 
     def loads(self, value):
-        return msgpack.loads(value)
+        return msgpack.unpackb(value, raw=False)
