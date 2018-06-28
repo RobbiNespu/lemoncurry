f7d79364996a321e7e8e2766307798ec73b64a48
Danielle McLean <dani@00dani.me>
2018-06-28 13:03:53 +1000

    Whoops, make sure ago actually emits the /correct/ relative timestamps using the right timezone

--- lemoncurry/jinja2/ago.py
+++ lemoncurry/jinja2/ago.py
@@ -3,4 +3,7 @@ from datetime import datetime
 
 
 def ago(dt: datetime) -> str:
+    # We have to convert the datetime we get to local time first, because ago
+    # just strips the timezone from a timezone-aware datetime.
+    dt = dt.astimezone()
     return human(dt, past_tense='{}', abbreviate=True)
