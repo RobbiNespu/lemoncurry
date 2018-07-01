6c9b6eb0612a90329daf2ae928b23e3919e00e10
Danielle McLean <dani@00dani.me>
2018-07-01 14:56:24 +1000

    Shrink the precision of 'ago' datetimes so they stay compact

--- lemoncurry/jinja2/ago.py
+++ lemoncurry/jinja2/ago.py
@@ -6,4 +6,4 @@ def ago(dt: datetime) -> str:
     # We have to convert the datetime we get to local time first, because ago
     # just strips the timezone from a timezone-aware datetime.
     dt = dt.astimezone()
-    return human(dt, past_tense='{}', abbreviate=True)
+    return human(dt, precision=1, past_tense='{}', abbreviate=True)
