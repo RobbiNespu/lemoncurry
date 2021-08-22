37d5a7a20d3f07aaa8a4041cd3e6092789c1e6d1
Danielle McLean <dani@00dani.me>
2021-08-22 23:24:58 +1000

    Do a pipenv update to get patched Django

--- lemoncurry/settings/test.py
+++ lemoncurry/settings/test.py
@@ -3,3 +3,6 @@ from .base import *
 ALLOWED_HOSTS = ['*']
 SECURE_SSL_REDIRECT = False
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
+MEDIA_URL = '/media/'
+STATIC_ROOT = path.join(BASE_DIR, 'media')
